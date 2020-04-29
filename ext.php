8cdc0fb64e91621cd0937747958c8ec6dc187889
kinerity <kinerity@yahoo.com>
2020-04-29 07:34:13 -0400

    Check for phpBB and PHP requirements.

--- ext.php
+++ ext.php
@@ -18,4 +18,17 @@ namespace kinerity\topicstarter;
  */
 class ext extends \phpbb\extension\base
 {
+	/**
+	 * Check whether or not the extension can be enabled.
+	 * The current phpBB version should meet or exceed
+	 * the minimum version required by this extension:
+	 *
+	 * Requires phpBB 3.3.0 and PHP 7.1.3
+	 */
+	public function is_enableable()
+	{
+		$config = $this->container->get('config');
+
+		return phpbb_version_compare($config['version'], '3.3.0', '>=') && version_compare(PHP_VERSION, '7.1.3', '>=');
+	}
 }
